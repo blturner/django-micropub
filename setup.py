f8615d1c903fd9083fd3b418b58d2c8ef453f103
Benjamin Turner <benturn@gmail.com>
2022-05-06 18:00:09 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="micropub",
+    version="0.1",
+    description="A micropub server.",
+    url="http://github.com/blturner/django-micropub",
+    author="Benjamin Turner",
+    author_email="ben@benjaminturner.me",
+    license="MIT",
+    packages=["micropub"],
+    zip_safe=False,
+)
